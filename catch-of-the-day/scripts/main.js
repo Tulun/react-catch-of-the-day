59223a17805d99d7ac338a84ca7a9b11c7c68760
Tulun <jaykiraly@gmail.com>
2015-11-09 16:28:57 -0800

    Added some basic routing to app using ReactRoute.

--- catch-of-the-day/scripts/main.js
+++ catch-of-the-day/scripts/main.js
@@ -1,7 +1,12 @@
 
 var React = require('react');
 var ReactDOM = require('react-dom');
+var ReactRouter = require('react-router');
 
+var Router = ReactRouter.Router;
+var Route = ReactRouter.Route;
+var Navigation =ReactRouter.Navigation;
+var createBrowserHistory = require('history/lib/createBrowserHistory')
 
 var App = React.createClass({
 
@@ -56,12 +61,11 @@ var Inventory = React.createClass({
 
 var StorePicker = React.createClass({
   render : function() {
-    var name = "Jason"
     // JS comments here
     return (
       <form className="store-selector">
         {/* Creating the store */}
-        <h2> Please Enter A Store {name} </h2>
+        <h2> Please Enter A Store </h2>
         <input type ="text" ref="storeId" required />
         <input type="Submit" />
       </form>
@@ -69,5 +73,27 @@ var StorePicker = React.createClass({
   }
 });
 
+// Not found
 
-ReactDOM.render(<App/>, document.querySelector('#main'));
+var NotFound = React.createClass({
+  render : function() {
+    return (
+      <h1> Not Found! </h1>
+    )
+  }
+})
+
+/* 
+  Routes
+*/
+
+var routes = (
+  <Router history={createBrowserHistory()}>
+    <Route path="/" component={StorePicker} />
+    <Route path="/store/:storeId" component={App} />
+    <Route path="*" component={NotFound}/>
+  </Router>
+)
+
+
+ReactDOM.render(routes, document.querySelector('#main'));

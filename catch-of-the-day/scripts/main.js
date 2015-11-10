a562e7fcc38ec06ec0ff1133f58bf8e194c1c1f5
Tulun <jaykiraly@gmail.com>
2015-11-09 16:58:30 -0800

    Created basic event listener.

--- catch-of-the-day/scripts/main.js
+++ catch-of-the-day/scripts/main.js
@@ -6,7 +6,13 @@ var ReactRouter = require('react-router');
 var Router = ReactRouter.Router;
 var Route = ReactRouter.Route;
 var Navigation =ReactRouter.Navigation;
+
+
+// Mixin
+var History = ReactRouter.History;
 var createBrowserHistory = require('history/lib/createBrowserHistory')
+
+var h = require('./helpers');
 
 var App = React.createClass({
 
@@ -60,13 +66,22 @@ var Inventory = React.createClass({
 });
 
 var StorePicker = React.createClass({
+
+  mixins : [History],
+
+  goToStore : function(event) {
+    event.preventDefault();
+    // getting data from input
+    var storeId = this.refs.storeId.value;
+    this.history.pushState(null, '/store/' + storeId)
+    },
   render : function() {
     // JS comments here
     return (
-      <form className="store-selector">
+      <form className="store-selector" onSubmit={this.goToStore}>
         {/* Creating the store */}
         <h2> Please Enter A Store </h2>
-        <input type ="text" ref="storeId" required />
+        <input type ="text" ref="storeId" defaultValue={h.getFunName()} required />
         <input type="Submit" />
       </form>
     )
